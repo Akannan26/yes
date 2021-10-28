a52bcfd08f6a1eac1007114f90715bfe02d29378
Akannan26 <91959192+Akannan26@users.noreply.github.com>
2021-10-28 10:37:19 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,123 @@
-let _17 = 0
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.pause(1000)
-    basic.clearScreen()
+radio.onReceivedNumber(function (receivedNumber) {
+    DiceRoll = receivedNumber
+    diceRoll()
 })
-input.onButtonPressed(Button.AB, function () {
-    _17 = 0
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     for (let index = 0; index < 4; index++) {
-        led.plot(_17 % 5, 0)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        basic.pause(3000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(3000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
     }
 })
 input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    DiceRoll = randint(1, 6)
+    diceRoll()
+    radio.sendNumber(DiceRoll)
+})
+function diceRoll () {
+    soundExpression.spring.play()
     basic.showLeds(`
-        . # . # .
-        . # . # .
         . . . . .
         . # # # .
-        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
         `)
-    basic.pause(1000)
-    basic.clearScreen()
-})
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-    	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    if (DiceRoll == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
     }
-    if (input.buttonIsPressed(Button.B)) {
-    	
+    if (DiceRoll == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
     }
-})
+    if (DiceRoll == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (DiceRoll == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (DiceRoll == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (DiceRoll == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+}
+let DiceRoll = 0
+radio.setGroup(16)
